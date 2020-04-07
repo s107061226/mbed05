861855c50d9097addeb18778e11555e685b023fe
johnny <johnnyjohnny6201@gmail.com>
2020-04-07 23:33:44 +0800

    5_8

--- 5_8_EventQueue/main.cpp
+++ 5_8_EventQueue/main.cpp
@@ -9,6 +9,9 @@ int main() {
     queue.call_in(2000, printf, "called in 2 seconds\r\n");
     queue.call_every(1000, printf, "called every 1 seconds\r\n");
 
-    // events are executed by the dispatch method
+    // if do queue.dispatch(); this time will not execute the event below
+
+    queue.call_every(5000, printf, "Hello every 5 sec \r\n");
+    // excute the event in queue
     queue.dispatch();
 }
