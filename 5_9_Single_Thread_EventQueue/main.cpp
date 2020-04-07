16fb83bff55353bf00e8257903e65f05ce80b750
johnny <johnnyjohnny6201@gmail.com>
2020-04-08 01:33:58 +0800

    5_9

--- 5_9_Single_Thread_EventQueue/main.cpp
+++ 5_9_Single_Thread_EventQueue/main.cpp
@@ -1,29 +1,38 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
-DigitalOut led2(LED2);
-InterruptIn sw2(SW2);
-InterruptIn sw3(SW3);
-EventQueue queue(32 * EVENTS_EVENT_SIZE);
-Thread t;
+DigitalOut led1(LED1);                      // red led output
+DigitalOut led2(LED2);                      // green led output
+InterruptIn sw2(SW2);                       // SW2 interrupt signal
+InterruptIn sw3(SW3);                       // SW3 interrupt signal
+EventQueue queue(32 * EVENTS_EVENT_SIZE);   // set a queue to store 32 events
+Thread t;                                   // set a thread
 
+/* *****************************************
+ * print out sw2 triggered                 *
+ * interrupt ISR cannot use print          *
+ * it need to print out by calling queue   *
+ *******************************************/
 void led1_info() 
 {
     printf("led1 is triggered! \r\n");
 }
 
+/* ISR of sw2 */
 void Trig_led1()  
 {
     // Execute the time critical part first
     led1 = !led1;
-    // The rest can execute later in user context
+    // interrupt cannot use printf, call queue to print
     queue.call(led1_info);
 }
 
+/*********************************************
+ * ISR of sw3                                *
+ * it is in the queue, so it can use printf  *
+ *********************************************/
 void Trig_led2()  
 {
     led2 = !led2;
-    // Safe to use 'printf' in context of thread 't', while IRQ is not.
     printf("led2 is triggered! \r\n");
 }
 
