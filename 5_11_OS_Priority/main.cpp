d2d7d459e5f41eb5518320c5794ffff602c57aec
johnny <johnnyjohnny6201@gmail.com>
2020-04-08 03:09:49 +0800

    5_11

--- 5_11_OS_Priority/main.cpp
+++ 5_11_OS_Priority/main.cpp
@@ -6,6 +6,9 @@ DigitalOut led2(LED2);
 InterruptIn btn(SW2);
 EventQueue printfQueue;     // lower priority
 EventQueue eventQueue;      // normal priority
+Thread printfThread(osPriorityLow);
+Thread eventThread(osPriorityNormal);
+Ticker ledTicker;
 
 void blink_led2() 
 {
@@ -27,16 +30,12 @@ void btn_fall_irq()
 
 int main() 
 {
-    // low priority thread for calling printf()
-    Thread printfThread(osPriorityLow);
     printfThread.start(callback(&printfQueue, &EventQueue::dispatch_forever));
-    // normal priority thread for other events
-    Thread eventThread(osPriorityNormal);
     eventThread.start(callback(&eventQueue, &EventQueue::dispatch_forever));
-    // call blink_led2 every second, automatically defering to the eventThread
-    Ticker ledTicker;
     ledTicker.attach(eventQueue.event(&blink_led2), 1.0f);
     // button fall still runs in the ISR
     btn.fall(&btn_fall_irq);
-    while (1) {wait(1);}
+    while (1) {
+        wait(1);
+    }
 }
